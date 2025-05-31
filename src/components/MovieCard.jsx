d62d09c9270c35e359deb8d1466d0c3061acfbf0
thisisfaizanali <faizanaliwhb@gmail.com>
2025-05-31 17:52:39 +0530

    ix: update image paths for MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,19 +9,18 @@ const MovieCard = ({
         src={
           poster_path
             ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-            : "../public/no-movie.png"
+            : "/no-movie.png"
         }
-        alt="title"
+        alt={title}
       />
       <div className="mt-4">
         <h3>{title}</h3>
         <div className="content">
           <div className="rating">
-            <img src="../public/star.svg" alt="Star Icon" />
+            <img src="/star.svg" alt="Star Icon" />
             <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
           <span>•</span>
-
           <p className="lang">{original_language}</p>
           <span>•</span>
           <p className="year">
