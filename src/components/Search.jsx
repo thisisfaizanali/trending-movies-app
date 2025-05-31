93262a60bf78c7ee4666053d4b935e54870928b5
thisisfaizanali <faizanaliwhb@gmail.com>
2025-05-31 17:53:11 +0530

    fix: improve formatting and correct image source path in Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Search = ({searchTerm, setSearchTerm}) => {
+const Search = ({ searchTerm, setSearchTerm }) => {
     return (
         <div className="search">
             <div>
-                <img src = "../../public/search.svg" alt = "search"/>
+                <img src="/search.svg" alt="Search Icon" />
                 <input
-                type = "text"
-                placeholder = "Search through thousands of movies"
-                value={searchTerm}
-                onChange = {(e) => setSearchTerm(e.target.value)}/>
+                    type="text"
+                    placeholder="Search through thousands of movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)} />
             </div>
         </div>
     )
